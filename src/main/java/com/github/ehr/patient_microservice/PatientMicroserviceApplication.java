4304ca71be0b464718c848889a61033f0a0d0054
georvic <alexanderstower@gmail.com>
2019-04-18 20:58:23 -0400

    Issue #5: support for a discovery service based on Consul has been added

--- src/main/java/com/github/ehr/patient_microservice/PatientMicroserviceApplication.java
+++ src/main/java/com/github/ehr/patient_microservice/PatientMicroserviceApplication.java
@@ -3,7 +3,9 @@ package com.github.ehr.patient_microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.github.ehr.patient_microservice"})
 public class PatientMicroserviceApplication {
